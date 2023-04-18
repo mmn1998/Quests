d7014014f3ce3a468caaaa7a0bdf4313db98ce83
M.nomani <M.nomani@NOMANI-M>
2023-04-18 10:58:22 +0330

    init

---
+++ QueraQuests/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QueraQuests.SandTowers;
+using System;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+#region Sand Towers
+#region Input 1
+//int totalTowers = 3;
+//List<long> towersHeights = new List<long> { 1, 2, 3 };
+#endregion
+#region Input 2
+int totalTowers = 4;
+List<long> towersHeights = new List<long> { 2,1,3,2 };
+#endregion
+var sandTowers = new SandTowers(TotalTowers: totalTowers, TowerHeights: towersHeights);
+Console.WriteLine(sandTowers.CalculateMostBlocksForSortation());
+#endregion
---
+++ QueraQuests/SandTowers/SandTowers.Solution.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace QueraQuests.SandTowers;
+
+internal class SandTowers
+{
+    private readonly int? _totalTowers;
+    private readonly List<long>? _towerHeights;
+
+    public SandTowers(int? TotalTowers = null, List<long>? TowerHeights = null)
+    {
+        _totalTowers = TotalTowers;
+        _towerHeights = TowerHeights;
+    }
+    public int CalculateMostBlocksForSortation()
+    {
+        int maxBlocksNeededForSortation = 0;
+
+        try
+        {
+            if ((_totalTowers is null) || (_towerHeights is null) || (_totalTowers != _towerHeights.Count))
+            {
+                throw new Exception();
+            }
+            maxBlocksNeededForSortation = Proccess();
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return maxBlocksNeededForSortation;
+    }
+    int Proccess()
+    {
+        int totalBlocks = 0;
+        List<long> proccessedHeights = new List<long>();
+        for (int i = 0; i < _towerHeights?.Count; i++)
+        {
+            if (i == 0)
+            {
+                proccessedHeights.Add(_towerHeights[i]);
+                totalBlocks++;
+            }
+            else if (_towerHeights?[i - 1] < _towerHeights?[i])
+            {
+                if (proccessedHeights.Contains(_towerHeights[i]))
+                {
+                    continue;
+                }
+                else
+                {
+                    proccessedHeights.Add(_towerHeights[i]);
+                    totalBlocks++;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return totalBlocks;
+
+    }
+}
